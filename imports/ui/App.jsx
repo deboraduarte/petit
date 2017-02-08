539c9d3f77767dd6aefddbae7cf539c4ddbab1bb
Debora Duarte <debycardu@gmail.com>
2017-02-08 13:48:21 -0200

    Fix: delete AccountsUIWraoper component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import AccountsUIWrapper from './accountsUIWrapper.jsx';
 
 export default class App extends Component{
     render(){
         return(
             <div className="container">
                 <header itemscope itemtype="http://schema.org/Organization">
-                    <div class="logo-header col-xs-10 col-sm-10 col-lg-6 col-md-6 col-md-offset-4 col-lg-offset-4 col-sm-offset-3 col-xs-offset-3" itemprop="logo"></div>
+                    <div className="logo-header col-xs-10 col-sm-10 col-lg-6 col-md-6 col-md-offset-4 col-lg-offset-4 col-sm-offset-3 col-xs-offset-3" itemprop="logo"></div>
                 </header>
                 
-                <form action="submit" class="login-form col-xs-12 col-sm-12 col-md-6 col-xl-6  col-lg-offset-3 col-md-offset-3" >
-                    <input type="text" class="form-control col-xs-12 col-sm-12 col-md-6 col-xl-6" value="E-mail" />
-                    <input type="password" class="form-control col-xs-12 col-sm-12 col-md-6 col-xl-6" value="123456" />
-                    <button type="submit" class="btn btn-primary btn-lg col-xs-12 col-sm-12 col-md-6 col-xl-6 col-md-offset-3 col-lg-offset-3"> Entrar </button>
-                    <a href="#" class="signup-link col-xs-12 col-sm-12 col-lg-6 col-md-6 col-lg-offset-3 col-md-offset-3"> Não é Cadastrado? Crie uma conta agora! </a>
+                <form action="submit" className="login-form col-xs-12 col-sm-12 col-md-6 col-xl-6  col-lg-offset-3 col-md-offset-3" >
+                    <input type="text" className="form-control col-xs-12 col-sm-12 col-md-6 col-xl-6" value="E-mail" />
+                    <input type="password" className="form-control col-xs-12 col-sm-12 col-md-6 col-xl-6" value="123456" />
+                    <button type="submit" className="btn btn-primary btn-lg col-xs-12 col-sm-12 col-md-6 col-xl-6 col-md-offset-3 col-lg-offset-3"> Entrar </button>
+                    <a href="#" className="signup-link col-xs-12 col-sm-12 col-lg-6 col-md-6 col-lg-offset-3 col-md-offset-3"> Não é Cadastrado? Crie uma conta agora! </a>
                 </form>
             </div>
         );
